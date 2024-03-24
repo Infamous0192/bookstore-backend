9da91f8a942057b67e1f34540f007e714a845953
Dwa Meizadewa <infamous0192@gmail.com>
2024-03-24 16:01:57 +0800

    feat: add verify token

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,8 +1,24 @@
-import { Controller, Post, Body, HttpStatus, HttpCode } from '@nestjs/common';
-import { ApiBadRequestResponse, ApiTags } from '@nestjs/swagger';
+import {
+  Controller,
+  Post,
+  Body,
+  HttpStatus,
+  HttpCode,
+  Request,
+  Get,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiBadRequestResponse, ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { LoginDTO, RegisterDTO } from 'src/dto';
+import { AuthGuard } from 'src/guards';
 import { AuthService } from 'src/services';
-import { ErrorResponse, GeneralResponse, Authorized } from 'src/types';
+import {
+  ErrorResponse,
+  GeneralResponse,
+  Authorized,
+  User,
+  Creds,
+} from 'src/types';
 import { ApiGeneralResponse } from 'src/utils/decorators';
 
 @ApiTags('Auth')
@@ -11,6 +27,16 @@ import { ApiGeneralResponse } from 'src/utils/decorators';
 })
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Get('me')
+  @HttpCode(HttpStatus.OK)
+  @ApiGeneralResponse(User, HttpStatus.OK)
+  @ApiBadRequestResponse({ type: ErrorResponse })
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  async validate(@Request() request): Promise<Creds> {
+    return request['user'];
+  }
 
   @Post('login')
   @HttpCode(HttpStatus.OK)
